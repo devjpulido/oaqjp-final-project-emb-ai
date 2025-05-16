c0a9341ff0166a352637de6472f55eb933f3ec4a
Juan Pulido <jpul0007@student.monash.edu>
2025-05-16 03:14:09 -0400

    Task 6: Web deployment of the application using Flask

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/emotionDetector', methods=['GET'])
+def emotion_detector():
+
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+
+    if not response:
+        return "Invalid input or error occurred during processing."
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    result = (f"For the given statement, the system response is "
+              f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+              f"'joy': {joy} and 'sadness': {sadness}. "
+              f"The dominant emotion is {dominant_emotion}.")
+
+    return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
